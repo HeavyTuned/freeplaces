8514108c8327e2f374b93348d6cb5f8011181df9
Heavy Tuned <it@heavy-tuned.de>
2017-09-13 13:25:52 +0200

    gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,7 @@ module.exports = function (grunt) {
                 files: {
 
 
-                    'base.min.js': [
+                    'ui/base.min.js': [
                         'ui/src/*.js'
                     ]
                 }
